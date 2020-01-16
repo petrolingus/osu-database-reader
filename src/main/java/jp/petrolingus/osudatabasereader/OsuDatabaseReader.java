138f23bd16fa379384a5d482e76b082c7fd54279
Petrolingus <petrolingus@gmail.com>
2020-01-16 08:34:53 +0300

    add read() in OsuDatabaseReader

--- src/main/java/jp/petrolingus/osudatabasereader/OsuDatabaseReader.java
+++ src/main/java/jp/petrolingus/osudatabasereader/OsuDatabaseReader.java
@@ -1,4 +1,25 @@
 package jp.petrolingus.osudatabasereader;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class OsuDatabaseReader {
+
+    ByteBuffer byteBuffer;
+
+    public void read(String path) {
+
+        try {
+            byte[] buffer = Files.readAllBytes(Paths.get(path));
+            byteBuffer = ByteBuffer.wrap(buffer);
+            byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
