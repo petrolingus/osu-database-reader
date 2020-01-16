3834d00425783f50b528f40b6fe9180de8f3f108
Petrolingus <petrolingus@gmail.com>
2020-01-16 09:57:56 +0300

    add logToFile() in OsuDatabaseReader

--- src/test/java/jp/petrolingus/osudatabasereader/OsuDatabaseReaderTest.java
+++ src/test/java/jp/petrolingus/osudatabasereader/OsuDatabaseReaderTest.java
@@ -2,6 +2,13 @@ package jp.petrolingus.osudatabasereader;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import static org.junit.Assert.*;
 
 public class OsuDatabaseReaderTest {
@@ -16,9 +23,30 @@ public class OsuDatabaseReaderTest {
         OsuDatabase actual = OsuDatabaseReader.read("D:\\MyOsuList\\osu!.db");
 
         System.out.println(actual);
+        logToFile(actual);
 
         assertEquals(expected, actual);
 
     }
 
+    public void logToFile(OsuDatabase obj) {
+        String content = Arrays
+                .stream(OsuDatabase.class.getDeclaredFields())
+                .map(field -> {
+                    try {
+                        field.setAccessible(true);
+                       String name = field.getName();
+                       String value = Objects.toString(field.get(obj));
+                       return name + ": " + value;
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.joining(System.lineSeparator()));
+        try {
+            Files.write(Paths.get("logs/log.txt"), content.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
